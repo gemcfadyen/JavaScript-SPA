dc672fad420d391ebdc8388acff9b3f0deee2165
gemcfadyen <gemcfadyen@hotmail.co.uk>
2013-12-18 12:33:49 +0000

    added the bind functionality so that when adding an animal, it displays immediately on the webpage

--- start/js/lib/bind.js
+++ start/js/lib/bind.js
@@ -1,15 +1,33 @@
 define([], function () {
+
+    function bindElement(element, data) {
+        element.addEventListener("change", function () {   //look for the change event
+            data[element.id] = element.value;
+        });
+    }
+
+
     function bindForm(form, data) {
         if (!data.watch) {
             throw "Data for binding must be observable";
         }
 
+        //does the form have a field for that property, if it does set the field to the value it has at present
+        //change to the object is being reflected in the form
         data.watch(function (propertyName) {
             if (form[propertyName]) {
                 form[propertyName].value = data[propertyName];
             }
         });
+
+        var inputs = form.querySelectorAll("input"); //iterate through form's input fields
+        console.log(inputs);
+        for (var i = inputs.length - 1; i >= 0; i--) {
+          
+            bindElement(inputs[i], data);
+        }
     }
+
 
     return {
         form: bindForm
--- start/test/lib/bindSpec.js
+++ start/test/lib/bindSpec.js
@@ -2,16 +2,32 @@ define(["lib/bind", "lib/observable"], function (bind, observable) {
 
     describe("form binding", function () {
 
-        it("should update form with object changes", function () {
-            var html = '<form><input type ="text" id = "name"></form>';
+        function makeForm(){
+              var html = '<form><input type ="text" id = "name"></form>';
             var div = document.createElement("div");
             div.innerHTML = html;
-            var form = div.firstChild;
-
+            return div.firstChild;
+        }
+        
+        it("should update form with object changes", function () {
+            var form = makeForm();
             var data = observable.wrap({ name: "" });
             bind.form(form, data);
             data.name = "Pumba";
             expect(form.name.value).toEqual("Pumba");
         });
+        
+        it("should update object with form changes", function(){  
+            var form = makeForm();
+            var data = observable.wrap({name: ""});
+            bind.form(form, data);
+            //we have changed form.name.value programatically here to ensure it is reflected in the underlying data object
+            form.name.value = "Timon";
+            var change = document.createEvent("UIEvents");
+            change.initEvent("change", true, true);
+            form.name.dispatchEvent(change);
+            expect(data.name).toEqual("Timon");
+        });
+        
     });
 });
