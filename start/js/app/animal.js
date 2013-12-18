295e4ccb5e598531c869c7dd9fb844e230a1f6fd
gemcfadyen <gemcfadyen@hotmail.co.uk>
2013-12-18 14:23:07 +0000

    Animal characteristics can be edited, which writes back to the generated Json file

--- start/js/app/animal.js
+++ start/js/app/animal.js
@@ -1,13 +1,23 @@
 define(["lib/route", "lib/html", "lib/http", "lib/observable", "lib/bind"], function (route, html, http, observable, bind) {
     var _id, _animal;
     
+    function save(){
+        http("PUT", "/animals", JSON.stringify(_animal));
+    }
+    
     function setup(element) {
         http("GET", "/animals/" + _id).then(
         function(json){
             _animal = JSON.parse(json);
+            _animal.species = _animal.species || "";
             var model = observable.wrap(_animal);
             var form = element.querySelector("#animal");
             bind.form(form, model);
+            
+            form.addEventListener("submit", function(e){
+                e.preventDefault();
+                save();
+            });
         });
     }
 
@@ -18,6 +28,7 @@ define(["lib/route", "lib/html", "lib/http", "lib/observable", "lib/bind"], func
     });
 
     return {
-        setup: setup
+        setup: setup,
+        save: save
     };
 });
